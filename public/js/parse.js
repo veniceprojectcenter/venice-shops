f6aedccc4dc83dd20a282db998ecd866378dcb6d
sheehy98 <sheehy98@gmail.com>
2021-10-05 01:59:39 -0400

    local json commit

---
+++ public/js/parse.js
@@ -0,0 +1,9 @@
+const fs = require('fs')
+
+fs.readFile('./data/store_locations.json', 'utf8', (err, jsonString) => {
+    if (err) {
+        console.log("File read failed:", err)
+        return
+    }
+    console.log('File data:', jsonString) 
+})
