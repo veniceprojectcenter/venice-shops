81af18790f8ca1080b4daf34876d95f6e47b0abe
sheehy98 <sheehy98@gmail.com>
2021-10-07 14:10:48 -0400

    init filter

--- public/js/parse.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs')
-
-fs.readFile('./data/store_locations.json', 'utf8', (err, jsonString) => {
-    if (err) {
-        console.log("File read failed:", err)
-        return
-    }
-    console.log('File data:', jsonString) 
-})
