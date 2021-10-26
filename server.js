dc41a16e196fee5a38a819a72709562107ba6c54
sheehy98 <sheehy98@gmail.com>
2021-10-26 08:25:55 +0200

    major commit 10/26

--- server.js
+++ server.js
@@ -1,15 +1,69 @@
 const express = require("express"),
   app = express(),
+  bodyparser = require("body-parser"),
   favicon = require("serve-favicon"),
+  mongodb = require("mongodb"),
   port = 3003;
-  tweakpane = require("tweakpane")
+
+const uri = "mongodb+srv://mapuser:mapuser@cluster0.0k894.mongodb.net/"
+
+const client = new mongodb.MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+let collection = null;
+
+client.connect(err => {
+  collection = client.db("VeniceShops").collection("MapsFeatures")
+})
 
 app.use(express.static("public"));
-app.use(favicon(__dirname + "/public/assets/WPI.ico"));
+app.use(favicon(__dirname + "/public/assets/gondola.ico"));
+app.use(bodyparser.json());
 
 app.get("/", function(request, response) {
   response.sendFile("/index.html");
 });
 
-app.listen(process.env.PORT || port);
+app.get("/load", function(request, response) {
+  collection
+    .find({})
+    .toArray()
+    .then(result => response.json(result));
+})
+
+app.post("/add", function(request, response) {
+  const newInfo = request.body.info;
+  const json = {
+    store_name: request.body.name,
+    store_type: request.body.store,
+    group_type: request.body.group,
+    year_collected: request.body.year,
+    image_url: request.body.image
+  };
+  newInfo.push(json);
+  collection.updateOne(
+    { _id: mongodb.ObjectId(request.body._id) },
+    { $set: { info: newInfo }}
+  )
+  .then(collection.find({}).toArray())
+  .then(result => response.json(result));
+})
+
+app.post("/edit", function(request, response) {
+  const newInfo = request.body.info;
+  const json = {
+    store_name: request.body.name,
+    store_type: request.body.store,
+    group_type: request.body.group,
+    year_collected: request.body.year,
+    image_url: request.body.image
+  };
+  newInfo[request.body.index] = json;
+  collection.updateOne(
+    { _id: mongodb.ObjectId(request.body._id) },
+    { $set: { info: newInfo }}
+  )
+  .then(collection.find({}).toArray())
+  .then(result => response.json(result));
+})
+
+app.listen(port);
 
